a540c3da8b7ff4aa5b1e922d3b3d3459c1bf9fc4
Navid Istanbullu <navido972@gmail.com>
2025-09-09 08:55:12 +0200

    creating project structure and config files

---
+++ java/ConfigTemplate.java
@@ -0,0 +1,7 @@
+package java;
+
+public class ConfigTemplate {
+    public static final String AID = "insert-aid-in-here";
+    public static final String API_TOKEN = "insert-api-token-in-here";
+    public static final String REQUEST_URL = "https://sandbox.piano.io/api/v3/publisher/user/list";
+}
